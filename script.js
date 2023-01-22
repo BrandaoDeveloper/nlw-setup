a78c3f3c8244a35311bf0478b45686ed7f4b037d
Leonardo Brandao <brandao.developerweb@gmail.com>
2023-01-22 13:32:49 -0300

    Add first animation in website

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
-const button = document.querySelector('header button')
+const button = document.querySelector("header button")
 
-button.addEventListener('click', add)
+button.addEventListener("click", add)
 form.addEventListener("change", save)
 
 function add() {
-  const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExist = nlwSetup.dayExists(today)
 
+  if (dayExist) {
+    alert("Dia já inlcuso 🛑")
+    return
+  }
 
-if (dayExist) {
-  alert("Dia já inlcuso 🛑")
-return
+  alert("Adicionado com sucesso 🍀")
+  nlwSetup.addDay(today)
 }
 
-alert('Adicionado com sucesso 🍀')
-nlwSetup.addDay(today)
-}
-
-function save () {
-  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
+
+TweenMax.to(document.getElementsByClassName("heading"), 4, {
+  "--split": "100%",
+  repeat: -1,
+  repeatDelay: 1,
+  yoyo: true,
+  ease: "power4.inOut",
+})
